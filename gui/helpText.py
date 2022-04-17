a35ea8a01d657202b35cd003a8544d0d17291ab6
Absolut-AK <absolutGit@gmail.com>
2022-04-17 20:43:12 +0800

    setting everying up with sqlite

---
+++ gui/helpText.py
@@ -0,0 +1,16 @@
+def helps():
+    helpText ="""
+    -starter (pick a class, This is THE first thing you need to do to play...) \n
+    -quests (This will show how to get money)\n
+    -upgrade 'artifact' (This is to improve the stats of the artifact randomly, do -upgrade weapon as an example)\n
+    -profile (This is to check your statistics)\n
+    -artifacts (This is to check what artifacts you own and what is the stats on the artifact)\n
+    -sell (This is to sell items you have found with -find)\n
+    -find 'location' (Use to find items that are used for crafting and brewing but it's not in the game, rn it's only for money by selling)\n
+    -inventory (to see your items)\n
+    -location (to see all location for -find)\n
+    -heal (This is for restoring your health)\n
+    -guild (This is needed to enter -dungeon)
+    -dungeon 'floor number' (the higher the floor the harder the boss, min floor 1, max floor 100)
+    """
+    return helpText
---
+++ main.py
@@ -0,0 +1,63 @@
+import os
+import discord
+from discord.ext import commands
+import sqlite3
+import contextlib
+
+from gui.helpText import helps
+#sqlite connection/function
+con = sqlite3.connect('mainData.db')
+cur = con.cursor()
+#discord connection
+client = discord.Client()
+client = commands.Bot(command_prefix='-')
+client.remove_command('help')
+
+#commands
+@client.event
+async def on_ready():
+    await client.change_presence(activity=discord.Game(name="-help for information"))
+    with con:
+        cur.execute('CREATE TABLE IF NOT EXISTS user(id INTEGER, coin INTEGER)')
+        cur.execute('SELECT coin FROM user WHERE id = 1')
+        for i in cur.fetchall():
+            print(i[0])
+        cur.close
+    # with con:
+    #     cur = con.cursor()
+    #     cur.execute('CREATE TABLE IF NOT EXISTS user(id integer, coin integer, bank INTEGER)')
+    #     con.commit()
+
+
+@client.command()
+async def coinflip(ctx, ammount=0, HorT=None):
+    id = ctx.message.author.id
+    # if coin >= 10 and  coin >= ammount:
+    #     if HorT != None:
+    #         HorT = HorT.lower()
+    #     if ammount < 10  or ammount > 100 or HorT == None:
+    #         await ctx.send("Choose your bet(min 10, max 100) and guessing side(-coinflip 10 tails or heads)")
+    #     elif HorT == 'heads' or HorT == 'tails':
+    #         await ctx.send(coinFlip(ammount, HorT, id))
+    #     else:
+    #         await ctx.send("Choose your bet(min 10, max 100) and guessing side(-coinflip 10 tails or heads)")
+    # else:
+    #     await ctx.send("You don't have enough coins. <(￣︶￣)>")
+
+@client.command()
+async def help(ctx):
+    text = helps()
+    embed = discord.Embed(title="Tutorial for Yuki's DiscordBot", description=text)
+    embed.set_footer(text="This is yuki's beta version of the game, there are a lot missed future, it took 3 weeks, I think, I forgot... well have fun ig")
+    await ctx.send(embed=embed)
+
+@client.command()
+async def starter(ctx):
+    id = ctx.message.author.id
+
+#path to YukiChanBot's token and executes it
+path = "..\\YukiChanToken"
+os.chdir(path)
+with open('token.txt', 'r') as f:
+    token = f.read()
+    client.run(token)
